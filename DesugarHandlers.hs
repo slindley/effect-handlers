634702f4fa835490f4a42528d5750d3fd9b8ebe8
sam <Sam.Lindley@ed.ac.uk>
2012-10-27 23:56:45 +0100

    tweak comments

--- DesugarHandlers.hs
+++ DesugarHandlers.hs
@@ -1,3 +1,144 @@
+{- TODO:
+
+  * Generate type signatures for operation functions.
+
+  * Syntax for handler extension?
+
+  * McBride handlers?
+
+  * Closure conversion? Perhaps not feasible using Template Haskell.
+
+  * The curly brace syntax for specifying the operations defined in a
+  handler quasiquote is probably suboptimal. Consider alternatives.
+ -}
+
+{- Examples -}
+{-
+  State operations:
+
+    [operation|Get s : s|]
+    [operation|Put s : s -> ()|]
+
+  These elaborate to:
+
+    data Get s = Get
+    type instance Return (Get s) = s
+    get = doOp Get
+
+    newtype Put s = Put s
+    type instance Return (Put s) = ()
+    put s = doOp (Put s)
+
+  If one of the parameters to an operation has a fancy type then it is
+  sometimes necessary to add a type annotation on the generated
+  operation function. This is perfectly possible. We might improve
+  usability by outputting a type-signature (as it should always be
+  fully known anyway).
+
+  A non-forwarding state handler:
+
+    [handler|StateHandler s a : {Get s, Put s} -> s -> a  where
+      clause (StateHandler s) Get k = k (StateHandler s) s
+      clause _ (Put s) k = k (StateHandler s) ()
+    |]
+
+  This elaborates to:
+
+    newtype StateHandler s a = StateHandler s
+    type instance Result (StateHandler s a) = a
+    instance (StateHandler s a `Handles` Get s) where
+      clause (StateHandler s) Get k = k (StateHandler s) s
+    instance (StateHandler s a `Handles` Put s) where
+      clause _ (Put s) k = k (StateHandler s) ()
+
+  A forwarding state handler:
+
+    [handler|forward h.FStateHandler s a : {Get s, Put s} -> s -> a where
+      clause (FStateHandler s) Get k = k (FStateHandler s) s
+      clause _ (Put s) k = k (FStateHandler s) ()
+    |]
+
+  This prepends h to the list of FStateHandler's type variables yielding:
+
+    newtype FStateHandler h s a = FStateHandler s
+    type instance Result (FStateHandler h s a) = a
+    instance (FStateHandler h s a `Handles` Get s) where
+      clause (FStateHandler s) Get k = k (FStateHandler s) s
+    instance (FStateHandler h s a `Handles` Put s) where
+      clause _ (Put s) k = k (FStateHandler s) ()
+
+  and additionally generates the following forwarding instances:
+
+    instance (h `Handles` op) => (PVHandler h a `Handles` op) where
+      clause h op k = doOp op >>= k h
+    instance (h `PolyHandles` op) => (PVHandler h a `PolyHandles` op) where
+      polyClause h op k = polyDoOp op >>= k h
+
+  A polymorphic operation:
+
+    [operation|Failure : forall a.a|]
+
+  This elaborates to:
+
+    data Failure a = Failure
+    type instance Return (Failure a) = a
+    failure = doPolyOp Failure
+
+  A polymorphic handler:
+
+    [handler|MaybeHandler a : {Failure} -> Maybe a where
+       polyClause _ Failure k = Nothing
+    |]
+
+  This elaborates to
+
+    newtype MaybeHandler a = MaybeHandler
+    type instance Result (MaybeHandler a) = a
+    instance (MaybeHandler a `PolyHandles` Failure) where
+      polyClause _ Failure k = Nothing
+
+  The collection of operations in the curly braces includes only those
+  operations whoses clauses are defined up-front. Further clauses can
+  be added later using explicit instances of the Handles type
+  class. Sometimes we have to do this when we require an explicit
+  class constraint.
+
+  We might consider adapting the sugar to inline handler parameters as
+  curried arguments to the clauses and the continuation. For instance:
+
+    [handler|StateHandler s a : {Get s, Put s} -> s -> a where
+      clause s Get     k = k s s
+      clause s (Put s) k = k s ()
+    |]
+
+  would elaborate to:
+
+    newtype StateHandler s a = StateHandler s
+    type instance Result (StateHandler s a) = a
+    instance (StateHandler s a `Handles` Get s) where
+      clause (StateHandler s) Get k' = k s s
+        where
+          k s = k' (StateHandler s)
+    instance (StateHandler s a `Handles` Put s) where
+      clause _ (Put s) k' = k s ()
+        where
+          k s = k' (StateHandler s)
+
+  We might also generate a wrapper function for the handler:
+
+    stateHandler comp s r = handle comp (StateHandler s) r
+
+  Doing something like this for McBride handlers might make sense.
+
+  Perhaps we should change the syntax to make it the status of the
+  handled operations clearer:
+
+    [handler|StateHandler s a : s -> a handles {Get s, Put s} where
+      clause (StateHandler s) Get k = k (StateHandler s) s
+      clause _ (Put s) k = k (StateHandler s) ()
+    |]
+-}
+
 module DesugarHandlers where
 
 import ParseHandlers(parseOpDef, parseHandlerDef, HandlerDef)
