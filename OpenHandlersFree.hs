315acdfb71aa1528433035e642b9acaeb6756a03
sam <Sam.Lindley@ed.ac.uk>
2012-11-08 15:03:42 +0000

    Changed free monad implementation to use standardised argument order.

--- OpenHandlersFree.hs
+++ OpenHandlersFree.hs
@@ -13,7 +13,7 @@ type family Return op :: *
 type family Result h :: *
 
 class Handles h op where
-  clause :: op -> h -> (h -> Return op -> Result h) -> Result h
+  clause :: op -> (Return op -> h -> Result h) -> h -> Result h
 
 data Comp h a where
   Ret :: a -> Comp h a
@@ -30,18 +30,18 @@ instance Functor (Comp h) where
 doOp :: (h `Handles` op) => op -> Comp h (Return op)
 doOp op = Do op return
 
-handle :: Comp h a -> h -> (h -> a -> Result h) -> Result h
-handle (Ret v) h r   = r h v
-handle (Do op k) h r = clause op h (\h' v -> handle (k v) h' r)
+handle :: Comp h a -> (a -> h -> Result h) -> h -> Result h
+handle (Ret v) r h   = r v h
+handle (Do op k) r h = clause op (\v h' -> handle (k v) r h') h
 
-forward :: (Handles h op) => h' -> op -> (h' -> Return op -> Comp h a) -> Comp h a
-forward h op k = doOp op >>= k h
+forward :: (Handles h op) => op -> (Return op -> h' -> Comp h a) -> h' -> Comp h a
+forward op k h = doOp op >>= (\x -> k x h)
   
 data PureHandler a = PureHandler
 type instance Result (PureHandler a) = a
 
 handlePure :: Comp (PureHandler a) a -> a
-handlePure c = handle c PureHandler (const id)
+handlePure c = handle c (\x _ -> x) PureHandler
 
 data Get s = Get
 type instance Return (Get s) = s
@@ -54,9 +54,9 @@ put s = doOp (Put s)
 newtype StateHandler s a = StateHandler s
 type instance Result (StateHandler s a) = a
 instance (StateHandler s a `Handles` Get s) where
-  clause Get (StateHandler s) k = k (StateHandler s) s
+  clause Get k (StateHandler s) = k s (StateHandler s)
 instance (StateHandler s a `Handles` Put s) where
-  clause (Put s) _ k = k (StateHandler s) ()
+  clause (Put s) k _ = k () (StateHandler s)
 
 countTest =
     do {n <- get;
