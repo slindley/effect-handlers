5f9c8e610912c47c3fc3b4e110695681659a56e4
sam <Sam.Lindley@ed.ac.uk>
2012-12-10 15:19:42 +0000

    sugary

--- examples/StateShallow.hs
+++ examples/StateShallow.hs
@@ -13,29 +13,37 @@
     ScopedTypeVariables #-}
 
 import ShallowFreeHandlers
+import DesugarHandlers
 
-data Get = Get
-type instance Return Get = Int
-get = doOp Get
+[operation|Get s :: s|]
+[operation|Put s :: s -> ()|]
 
-newtype Put = Put Int
-type instance Return Put = ()
-put s = doOp (Put s)
+-- data Get = Get
+-- type instance Return Get = Int
+-- get = doOp Get
 
-newtype SimpleState a = SimpleState Int
-type instance Result (SimpleState a) = a
-type instance Inner (SimpleState a) = a
-instance (SimpleState a `Handles` Put) where
-  clause (Put s) k h = simpleState s (k ())
-instance (SimpleState a `Handles` Get) where
-  clause Get k (SimpleState s) = simpleState s (k s)
-simpleState s comp = handle comp (\x _ -> x) (SimpleState s)
-<<<<<<< HEAD
---simpleState' s comp = simpleState s (Comp (\k -> comp >>= k))
-=======
---simpleState' s comp = handle' comp (\x _ -> x) (SimpleState s)
---simpleState s comp = simpleState' s (lower comp)
->>>>>>> 8f8769e15f4fc58c2ec83eef8fb3b34844b648e5
+-- data Put = Put Int
+-- type instance Return Put = ()
+-- put s = doOp (Put s)
+
+[shallowHandler|
+  SimpleState s a :: s -> a
+    handles {Get s, Put s} where
+      Return  x     _ -> x
+      Get        k  s -> simpleState s (k s)
+      Put     s  k  _ -> simpleState s (k ())
+|]
+
+
+-- newtype SimpleState a = SimpleState Int
+-- type instance Result (SimpleState a) = a
+-- type instance Inner (SimpleState a) = a
+-- instance (SimpleState a `Handles` Put) () where
+--   clause (Put s) k h = simpleState s (k ())
+-- instance (SimpleState a `Handles` Get) () where
+--   clause Get k (SimpleState s) = simpleState s (k s)
+-- simpleState s comp = handle comp (\x _ -> x) (SimpleState s)
+-- --simpleState' s comp = simpleState s (Comp (\k -> comp >>= k))
 
 
 countH =
@@ -44,6 +52,6 @@ countH =
         else do {put (i-1); countH}}
 
 
-test5 = print (simpleState 10000000 countH)
+test5 = print (simpleState 100000000 countH)
 
 main = test5
