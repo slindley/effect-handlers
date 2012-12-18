63df9042f2ed0ce96078520b647ac02155809cdb
sam <Sam.Lindley@ed.ac.uk>
2012-12-18 11:20:36 +0000

    Actually use pre and post conditions!

--- ix/HandlerIx.hs
+++ ix/HandlerIx.hs
@@ -1,3 +1,5 @@
+{- Shallow handlers for indexed types using a free monad -}
+
 {-# LANGUAGE
     GADTs,
     TypeFamilies,
@@ -18,14 +20,14 @@ import MonadIx
 
 type family Return (op :: k -> *) :: k -> *
 
-type family Result (h :: *) :: k -> *
-type family Inner h :: *
-type family Post (h :: *) :: k
+type family Result (h :: k -> *) :: k -> *
+type family Inner  (h :: k -> *) :: *
+type family Post   (h :: k -> *) :: k
 
-class (((h :: *) `Handles` (op :: k -> *))) where
-  clause :: op pre -> (Return op :-> Comp h (Inner h := Post h)) -> h -> Result h pre
+class (((h :: k -> *) `Handles` (op :: k -> *))) where
+  clause :: op pre -> (Return op :-> Comp h (Inner h := Post h)) -> h pre -> Result h pre
 
-data Comp h (a :: (k -> *)) :: (k -> *) where
+data Comp (h :: k -> *) (a :: (k -> *)) :: (k -> *) where
   Ret :: a pre -> Comp h a pre
   Do  :: (h `Handles` op) => op pre -> (Return op :-> Comp h a) -> Comp h a pre
 
@@ -42,9 +44,12 @@ instance ApplicativeP (Comp h) where
 doOp :: (h `Handles` op) => op post -> Comp h (Return op) post
 doOp op = Do op returnIx
 
-handle :: Comp h (Inner h := Post h) pre -> (Inner h -> h -> Result h pre) -> h -> Result h pre
+handle :: Comp h (Inner h := Post h) pre -> (Inner h -> h pre -> Result h pre) -> h pre -> Result h pre
 handle (Ret (V x)) r h = r x h
 handle (Do op k)   r h = clause op k h
+
+handleInner :: Comp h (Inner h := Post h) pre -> h pre -> Result h pre
+handleInner (Do op k) h = clause op k h
 
 data FileState :: * where
   Open   :: FileState
@@ -82,7 +87,9 @@ readOpenFile = fGetC =>= \x -> case x of
 
 newtype Wrap (a :: *) (i :: k) = Wrap {unWrap :: IO a}
 
-data FH (a :: *) = RunFH | OpenFH Handle
+data FH (a :: *) (pre :: k) where
+     RunFH  ::           FH a 'Closed 
+     OpenFH :: Handle -> FH a 'Open
 type instance Result (FH a) = Wrap a
 type instance Inner (FH a)  = a
 type instance Post (FH a)   = 'Closed
@@ -97,15 +104,14 @@ runFH :: Comp (FH a) (a := 'Closed) 'Closed -> Wrap a 'Closed
 runFH m = handle m (\x _ -> Wrap (return x)) RunFH
 
 instance (FH a `Handles` FClose) where
-  clause (FClose (V ())) k (OpenFH h) = Wrap (hClose h >> unWrap (runFH (k (V ()))))
+  clause (FClose (V ())) k (OpenFH file) = Wrap (hClose file >> unWrap (runFH (k (V ()))))
 instance (FH a `Handles` FGetC) where
-  clause (FGetC (V ())) k (OpenFH h) =
+  clause (FGetC (V ())) k (OpenFH file) =
           Wrap
             (catch
-              (hGetChar h >>= \c   -> unWrap (openFH h (k (V (Just c)))))
-              (\(_ :: IOException) -> unWrap (openFH h (k (V Nothing)))))
+              (hGetChar file >>= \c -> unWrap (openFH file (k (V (Just c)))))
+              (\(_ :: IOException) -> unWrap (openFH file (k (V Nothing)))))
 openFH :: Handle -> Comp (FH a) (a := 'Closed) 'Open -> Wrap a 'Open
-openFH h m = handle m undefined (OpenFH h)
-
+openFH file m = handleInner m (OpenFH file)
 
 test1 = unWrap (runFH (fileContents "test.txt"))
