3041a14e443274821806fae5411a0d551691fa65
sam <Sam.Lindley@ed.ac.uk>
2013-02-18 18:02:42 +0000

    The beginnings of a very simple web server.

---
+++ examples/Web.hs
@@ -0,0 +1,102 @@
+{-# LANGUAGE TypeFamilies,
+    GADTs,
+    RankNTypes,
+    MultiParamTypeClasses,
+    QuasiQuotes,
+    FlexibleInstances,
+    FlexibleContexts,
+    OverlappingInstances,
+    UndecidableInstances,
+    ConstraintKinds,
+    TypeOperators,
+    DataKinds,
+    PolyKinds,
+    ScopedTypeVariables #-}
+
+import Handlers
+import TopLevel
+import DesugarHandlers
+
+type RequestType = String
+type URL = String
+
+[operation|AcceptConnection a :: Servlet a -> ()|]
+
+[operation|         EmitHeader      :: String -> String -> ()|]
+[operation|forall a.TerminateHeader :: ServletBody a -> a|]
+
+[operation|Emit            :: String ->           ()|]
+
+[operation|GetRequestType  :: RequestType|]
+[operation|GetURL          :: URL|]
+[operation|GetValueOf      :: String -> Maybe String|]
+
+type HeaderConstraints  h = ([handles|h {EmitHeader}|], [handles|h {TerminateHeader}|])
+type BodyConstraints    h = ([handles|h {Emit}|])
+type RequestConstraints h = ([handles|h {GetRequestType}|], [handles|h {GetURL}|], [handles|h {GetValueOf}|])
+
+type Servlet a = (HeaderConstraints h, BodyConstraints h, RequestConstraints h) => Comp h a
+type ServletBody a = (BodyConstraints h, RequestConstraints h) => Comp h a
+
+server :: ([handles|h {AcceptConnection a}|]) => Servlet a -> Comp h ()
+server p = 
+  do acceptConnection p;
+     server p
+
+
+-- parsing HTTP headers
+[operation|GetC :: Char|]
+[operation|PeekC :: Char|]
+[operation|forall a.Failure :: a|]
+
+getUntil :: ([handles|h {GetC}|]) => Char -> Comp h String
+getUntil c =
+  do c' <- getC
+     if c == c'
+       then return []
+       else do s <- getUntil c
+               return (c' : s)
+
+skipSpace :: ([handles|h {PeekC}|], [handles|h {GetC}|]) => Comp h ()
+skipSpace = do c <- peekC
+               if c == ' '
+                 then do getC; skipSpace
+                 else return ()
+
+nextLine :: ([handles|h {Failure}|], [handles|h {GetC}|]) => Comp h ()
+nextLine = do c <- getC
+              if c == '\n'
+                then return ()
+                else failure
+
+getHeader :: ([handles|h {Failure}|], [handles|h {GetC}|], [handles|h {PeekC}|]) => Comp h (Maybe (String, String))
+getHeader = do r <- peekC
+               if r == '\r'
+                 then do getC; nextLine; return Nothing
+                 else
+                   do
+                     key <- getUntil ':'
+                     skipSpace
+                     value <- getUntil '\r'
+                     nextLine
+                     return (Just (key, value))
+
+getFirstLine :: ([handles|h {Failure}|], [handles|h {GetC}|], [handles|h {PeekC}|]) => Comp h (String, String, String)
+getFirstLine = do method <- getUntil ' '
+                  path <- getUntil ' '
+                  version <- getUntil '\r'
+                  nextLine
+                  return (method, path, version)
+
+-- handling peek in terms of get
+[handler|
+  forward h handles {GetC}.
+    HandlePeek a :: Maybe Char -> a
+      handles {PeekC, GetC} where
+        Return x   _        -> return x
+        GetC     k Nothing  -> do c <- getC; k c Nothing
+        GetC     k (Just c) -> k c Nothing
+        PeekC    k Nothing  -> do c <- getC; k c (Just c)
+        PeekC    k (Just c) -> k c (Just c)
+|]
+handlePeek' comp = handlePeek Nothing comp
